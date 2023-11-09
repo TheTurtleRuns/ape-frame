8219122c6ccc39f5508361e8041daa59e7307e2f
15036670355 <15036670355@163.com>
2023-11-09 11:05:22 +0800

    包统一管理

---
+++ ape-common/ape-common-log/src/main/java/com/psj/log/LogAspect.java
@@ -0,0 +1,44 @@
+package com.psj.log;
+
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName LogAspect
+ * @Author Pengshj
+ * @Description 日志切面
+ * @Date 2023/11/9 10:33
+ **/
+@Aspect
+@Slf4j
+@Component
+@ConditionalOnProperty(name = {"log.aspect.enable"}, havingValue = "true", matchIfMissing = true)
+public class LogAspect {
+
+    @Pointcut("execution(* com.psj.*.controller.*Controller.*(..)) || execution(* com.psj.*.service.*Service.*(..))")
+    private void pointCut() {
+    }
+
+    @Around("pointCut()")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        Object[] reqArgs = pjp.getArgs();
+        String req = new Gson().toJson(reqArgs);
+        MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+        String methodName = methodSignature.getDeclaringType().getName() + "." + methodSignature.getName();
+        log.info("{},req:{}", methodName, req);
+        Long startTime = System.currentTimeMillis();
+        Object responseObj = pjp.proceed();
+        String resp = new Gson().toJson(responseObj);
+        Long endTime = System.currentTimeMillis();
+        log.info("{},response:{},costTime:{}", methodName, resp, endTime - startTime);
+        return responseObj;
+    }
+
+}
